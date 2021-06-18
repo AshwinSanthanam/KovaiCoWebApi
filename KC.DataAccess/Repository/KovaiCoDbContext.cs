0877ae7de41a7608c76841c0fab0a03de4be18ae
AshwinSanthanam <ashwinsanthanam098@gmail.com>
2021-06-18 23:07:11 +0530

    Repository Implementation

---
+++ KC.DataAccess/Repository/KovaiCoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using KC.Base.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KC.DataAccess.Repository
+{
+    public class KovaiCoDbContext : DbContext
+    {
+        public KovaiCoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ KC.DataAccess/Repository/Repository.cs
@@ -0,0 +1,38 @@
+﻿using KC.Base;
+using KC.Base.Models;
+using KC.Base.TransientModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KC.DataAccess.Repository
+{
+    public class Repository : IRepository
+    {
+        private readonly KovaiCoDbContext _dbContext;
+
+        public Repository(KovaiCoDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IQueryable<User> Users => throw new NotImplementedException();
+
+        public User DeleteUser(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User InsertUser(TransientUser transientUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User UpdateUser(long id, TransientUser transientUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
