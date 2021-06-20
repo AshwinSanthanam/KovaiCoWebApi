eff91d0f1c63190a5471926f078733ca71716bca
AshwinSanthanam <ashwinsanthanam098@gmail.com>
2021-06-20 10:43:52 +0530

    Revert "Revert "Social login support in user table""
    This reverts commit 9db16f945354149ff54ad150ba4ec7d28cabb6e9.

---
+++ KC.DataAccess/Migrations/20210620035853_SocialLoginSupportInUserTable.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KC.DataAccess.Migrations
+{
+    public partial class SocialLoginSupportInUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSocialLogin",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSocialLogin",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+        }
+    }
+}
